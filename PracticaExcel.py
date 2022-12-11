688b55e5f0b508da6690e1555599a6edbf75d6d1
Jero Rm <88847168+TheHakoDrako@users.noreply.github.com>
2022-12-11 13:44:25 -0500

    Update Practicas en Excel
    - Only Text

--- PracticaExcel.py
+++ PracticaExcel.py
@@ -1,7 +1,14 @@
-# Naturalmente se emplean muchas librerias a la hora 
-#
+""" 
+Naturalmente se emplean muchas librerias a la hora de escribir code, dado que tienen muchas funcionalidades.
+Pandas es una libreria que tiene muchas herramientas para realizar analisis de datos y manipularlos, por lo tanto, puede ser una forma de cargar datos de excel.
+"""
+
+# Llamamos/importamos la libreria:
 
 import pandas as pd
+
+# Definimos una funcion para cargar datos utilizando el método "pd.read_excel" que es de la libreria pandas
+# La funcion cargara los datos del archivo de excel (NombreDelArchivo) en un objeto de tipo "DataFrame"
 
 def cargarDatosExcel(NombreDelArchivo):
   df = pd.read_excel(NombreDelArchivo)
